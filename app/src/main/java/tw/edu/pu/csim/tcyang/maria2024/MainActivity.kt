cc052e76a8149169343e1a68f6adc86d5fd2c109
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-12-01 13:35:54 +0800

    v3_楊子青

--- app/src/main/java/tw/edu/pu/csim/tcyang/maria2024/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/maria2024/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,9 +17,15 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import tw.edu.pu.csim.tcyang.maria2024.ui.theme.Maria2024Theme
@@ -42,10 +49,36 @@ class MainActivity : ComponentActivity() {
 fun Exam(m:Modifier) {
     val activity = (LocalContext.current as? Activity)
 
+    var bkColor = arrayOf(
+        Color(0xff95fe95), Color(0xfffdca0f),
+        Color(0xfffea4a4), Color(0xffa5dfed))
+    var offset1 by remember { mutableStateOf(Offset.Zero) }
+    var offset2 by remember { mutableStateOf(Offset.Zero) }
+    var Number by remember { mutableStateOf(0) }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xff95fe95))
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDrag = { change, dragAmount -> offset2+=dragAmount},
+                    onDragStart = {
+                        offset1 = it
+                        offset2 = it },
+                    onDragEnd = {
+                        if (offset2.x >= offset1.x){
+                            Number ++
+                            if (Number>3){Number=0}
+                        }
+                        else{
+                            Number --
+                            if (Number<0){Number=3}
+                        }
+
+                    }
+                )
+            }
+            .background(bkColor[Number])
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
