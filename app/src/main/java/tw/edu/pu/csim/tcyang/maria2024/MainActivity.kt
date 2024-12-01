6a8c092ee5f2137aacf2b3df5949df1edd424f27
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-12-01 13:18:41 +0800

    v2_楊子青

--- app/src/main/java/tw/edu/pu/csim/tcyang/maria2024/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/maria2024/MainActivity.kt
@@ -1,15 +1,26 @@
 package tw.edu.pu.csim.tcyang.maria2024
 
+import android.app.Activity
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import tw.edu.pu.csim.tcyang.maria2024.ui.theme.Maria2024Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,6 +40,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Exam(m:Modifier) {
+    val activity = (LocalContext.current as? Activity)
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff95fe95))
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "2024期末上機考(資管四A楊子青)",
+                modifier = m
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.class_a),
+                contentDescription = "A班合照",
+            )
+
+            Text("遊戲持續時間 0 秒")
+            Text("您的成績 0 分")
+
+            Button(onClick = {
+                activity?.finish()
+            }) { Text("結束App") }
+        }
+    }
 }
 
